539cb11291efca517aaacb2e7a90f1520dfceeea
Ike Mattice <ike@Ikes-iMac.local>
2016-01-31 05:48:31 -0800

    fix routing error that broke page when reloaded.

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
 var ike = angular.module('ike', ['ui.router']);
 
 ike.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('projects', {
-        url:'',
-        views: {
-            'header': {
-                templateUrl:'html/header.html',
-                // controller:'HeaderCtrl'
-            },
-            'projects': {
-                templateUrl:'html/projects.html'
-            },
-            'footer': {
-                templateUrl:'html/footer.html'
-            },
-        }
-    }); //end projects state
 
-    $stateProvider.state('skills', {
-        url:'skills',
-        views: {
-            'header': {
-                templateUrl:'html/header.html',
-                // controller:'HeaderCtrl'
-            },
-            'skills': {
-                templateUrl:'html/skills.html'
-            },
-            'footer': {
-                templateUrl:'html/footer.html'
-            },
-        }
-    }); //end skills state
+    $stateProvider.state('home', {
+            url:'',
+            views: {
+                'header': {
+                    templateUrl:'html/header.html',
+                    // controller:'HeaderCtrl'
+                },
+                'projects': {
+                    templateUrl:'html/projects.html'
+                },
+                'footer': {
+                    templateUrl:'html/footer.html'
+                },
+            }
+        }); //end projects state
+
+    $stateProvider.state('projects', {
+            url:'/:projects',
+            views: {
+                'header': {
+                    templateUrl:'html/header.html',
+                    // controller:'HeaderCtrl'
+                },
+                'projects': {
+                    templateUrl:'html/projects.html'
+                },
+                'footer': {
+                    templateUrl:'html/footer.html'
+                },
+            }
+        }); //end projects state
+$stateProvider
+        .state('skills', {
+            url:'/:skills',
+            views: {
+                'header': {
+                    templateUrl:'html/header.html',
+                    // controller:'HeaderCtrl'
+                },
+                'skills': {
+                    templateUrl:'html/skills.html'
+                },
+                'footer': {
+                    templateUrl:'html/footer.html'
+                },
+            }
+        }); //end skills state
 
     $stateProvider.state('contact', {
-        url:'contact',
+        url:'/:contact',
         views: {
             'header': {
                 templateUrl:'html/header.html',
@@ -50,7 +67,7 @@ ike.config(function($stateProvider, $urlRouterProvider) {
     }); //end contact state
 
     $stateProvider.state('about', {
-        url:'about',
+        url:'/:about',
         views: {
             'header': {
                 templateUrl:'html/header.html',
@@ -65,6 +82,7 @@ ike.config(function($stateProvider, $urlRouterProvider) {
         }
     }); //end about state
 
+//$state.go('projects', [inherit], {reload: 'child.state'});
 
 
 });  //end config
